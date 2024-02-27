6f3be1b45d8d0e13eb1e009a5921cff2008c2352
slava <lobikova12@mail,ru>
2024-02-27 17:19:25 +0300

    add scroll to top

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/router'
 
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  next();
+});
+
 createApp(App).use(router).mount('#app')
